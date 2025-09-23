673d6415bb2905d657ff99bff0c42e8ae9df013a
Luca Elbio <lucajv03@hotmail.com>
2025-09-22 23:02:49 -0300

    Fix 3

--- app/controllers/Http/users_controller.ts
+++ app/controllers/Http/users_controller.ts
@@ -106,7 +106,7 @@ export default class UsersController {
       const user2 = await User.findBy('email', validatedEmail)
       console.log(user2) // Verificar que existe
 
-      const isValid = await hash.verify(user2!.password, 'tu-contraseña')
+      const isValid = await hash.verify(user2!.password, password)
       console.log(isValid) // Debería ser true
 
       // Usar el helper de autenticación para validar credenciales
